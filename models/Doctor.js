ce9b032cb15fe147a81faabd25bccb7e17ce6208
Bernardo Maggessi <bermaggessi2@yahoo.com>
2025-03-18 16:47:07 -0300

    back-end: CRUD básico

---
+++ models/Doctor.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose; // Correção na importação!
+
+const doctorSchema = new Schema({
+    doctorId: { type: mongoose.Schema.Types.ObjectId, default: () => new mongoose.Types.ObjectId(), unique: true },
+    name: {
+        type: String,
+        required: [true, "Doctor name is required"],
+    },
+    login: {
+        type: String,
+        required: [true, "Login is required"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    medicalSpecialty: { 
+        type: String,
+        required: [true, "Medical specialty is required"],
+    },
+    medicalRegistration: {
+        type: String,
+        required: [true, "Medical registration is required"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Email contact is required"],
+    },
+    phone: {
+        type: String,
+        required: [true, "Phone contact is required"],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Doctor = mongoose.model("Doctor", doctorSchema); // Nome do modelo no banco
+
+export default Doctor;
---
+++ routes/AppointmentController.js
@@ -0,0 +1,74 @@
+import express from "express";
+import AppointmentService from "../services/AppointmentService.js";
+
+
+const router = express.Router();
+
+router.get('/appointments', async (req, res) => {
+    try {
+        const appointments = await AppointmentService.getAllAppointment();
+        res.json(appointments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+router.get('/appointments/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const appointment = await AppointmentService.getAppointment(id);
+        if (!appointment) {
+            return res.status(404).json({ message: "Appointment not found" });
+        }
+        res.json(appointment);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+router.post('/appointments', async (req, res) => {
+    const { date, doctorId, patientId } = req.body;
+    try {
+        if (!date || !doctorId || !patientId) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+        const appointment = await AppointmentService.saveAppointment({ date, doctorId, patientId });
+        res.status(201).json(appointment);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+router.put('/appointments/:id', async (req, res) => {
+    const { id } = req.params;
+    const { date, doctorId, patientId } = req.body;
+    try {
+        const updatedAppointment = await AppointmentService.updateAppointment(id, { date, doctorId, patientId });
+        if (!updatedAppointment) {
+            return res.status(404).json({ message: "Appointment not found" });
+        }
+        res.json(updatedAppointment);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+router.delete('/appointments/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedAppointment = await AppointmentService.deleteAppointment(id);
+        if (!deletedAppointment) {
+            return res.status(404).json({ message: "Appointment not found" });
+        }
+        res.json({ message: "Appointment deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+export default router;
