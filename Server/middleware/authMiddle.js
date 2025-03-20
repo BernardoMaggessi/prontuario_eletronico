9e8b926ad57a4d85a5827c8b3bf9561d73bd2d03
Bernardo Maggessi <bermaggessi2@yahoo.com>
2025-03-20 10:40:38 -0300

    Corrigindo autenticação

--- Server/middleware/authMiddle.js
+++ Server/middleware/authMiddle.js
@@ -6,17 +6,20 @@ function verifyToken(req, res, next) {
         return res.status(401).json({ error: 'Access Denied! Token is missing.' });
     }
 
-    const token = authHeader.split(" ")[1]; // Pega apenas o token sem o "Bearer"
-    
+    const token = authHeader; // Pega o token diretamente
+
     if (!token) {
         return res.status(401).json({ error: 'Invalid Token format!' });
     }
 
     try {
-        const decoded = jwt.verify(token, 'you-secret-key'); // Verifica o token
+        // Verifique se a chave secreta é a mesma que usou na criação do token
+        console.log('Verificando token com a chave secreta:', process.env.JWT_SECRET_KEY || 'you-secret-key');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY || 'you-secret-key');
         req.doctorId = decoded.doctorId; // Associa o doctorId ao request
         next(); // Passa para a próxima função
     } catch (error) {
+        console.error('Token verification error:', error); // Log de erro para depuração
         return res.status(401).json({ error: 'Invalid token!' });
     }
 }
